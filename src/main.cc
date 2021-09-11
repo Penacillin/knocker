e242451d6710cb09190f975a08240c55af9deffa
Penacillin <Pentracillin@gmail.com>
2021-09-11 18:38:45 +1000

    initial commit

---
+++ src/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+}
