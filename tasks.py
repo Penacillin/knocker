b204c1b82393605c2d05784de2cdec7307bef0ca
Penacillin <Pentracillin@gmail.com>
2021-09-15 01:56:43 +1000

    clean up cmakes remove curlpp

---
+++ tasks.py
@@ -0,0 +1,13 @@
+from invoke import task
+
+@task
+def build(c, verbose=False):
+    c.run("cmake --configure  --build build")
+    build_cmd = "cmake --build build --target all"
+    if verbose:
+        build_cmd += " -v"
+    c.run(build_cmd)
+
+@task
+def clean(c):
+    build_cmd = "cmake --build build --target clean"
